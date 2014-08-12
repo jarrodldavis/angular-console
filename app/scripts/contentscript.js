10b0aa498b51166cc047f8d1a491d3f3cc062286
Jarrod Davis <github@jarrodldavis.com>
2014-08-12 13:36:15 -0600

    Adds basic injection
    Injector, $rootScope, and $scope selector

--- app/scripts/contentscript.js
+++ app/scripts/contentscript.js
@@ -1,3 +1,20 @@
 'use strict';
 
-console.log('\'Allo \'Allo! Content script');
+var element = document.createElement("script");
+element.innerHTML =
+    "if (window.angular) {" +
+    "    var ngAppElem = angular.element(document.querySelector('[ng-app]') || document);" +
+    "" +
+    "    window.injector = ngAppElem.injector();" +
+    "    window.inject = injector.invoke;" +
+    "    window.$rootScope = ngAppElem.scope();" +
+    "" +
+    "    Object.defineProperty(window, '$scope', {" +
+    "        get: function () {" +
+    "            var elem = angular.element(__commandLineAPI.$0);" +
+    "            return elem.isolateScope() || elem.scope();" +
+    "        }" +
+    "    });" +
+    "    console.info('Injected Angular injector, $rootScope, and $scope selector.');" +
+    "}";
+document.head.appendChild(element);
